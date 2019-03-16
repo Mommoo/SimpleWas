7ed7e36345e594e243e7c61ec058ec609b06ebb9
mommoo <dv.mommoo@gmail.com>
2019-03-16 23:33:13 +0900

    ## server.json 구조 변경  ServerConfiguration 클래스 구현 변경

--- src/main/java/com/mommoo/conf/ServerConfiguration.java
+++ src/main/java/com/mommoo/conf/ServerConfiguration.java
@@ -11,6 +11,9 @@ import java.io.IOException;
 import java.util.*;
 
 public class ServerConfiguration {
+    private static final String MAIN_LOG_PATH = "mainLogPath";
+    private static final String SERVER_SPEC = "serverSpec";
+
     private static final String THREAD_COUNT = "threadCount";
     private static final String SERVER_NAME = "serverName";
     private static final String PORT_NUMBER = "portNumber";
@@ -19,16 +22,26 @@ public class ServerConfiguration {
     private static final String INDEX_PAGE = "indexPage";
     private static final String ERROR_PAGE = "errorPage";
 
-    private static final String[] KEYS = {THREAD_COUNT, SERVER_NAME, PORT_NUMBER, DOCUMENT_PATH, LOG_PATH, INDEX_PAGE, ERROR_PAGE};
+    private static final String[] SERVER_SPEC_KEYS = {THREAD_COUNT, SERVER_NAME, PORT_NUMBER, DOCUMENT_PATH, LOG_PATH, INDEX_PAGE, ERROR_PAGE};
 
     private static final String CODE = "code";
     private static final String PAGE = "page";
 
-    private Map<Integer, List<ServerSpec>> serverSpecFinder = new HashMap<>();
+    private final String mainLogPath;
+    private final Map<Integer, List<ServerSpec>> serverSpecFinder = new HashMap<>();
 
     public ServerConfiguration(String configFilePath) throws IOException, ClassCastException, ParseException, JSONKeyNotFoundException {
         FileReader fileReader = new FileReader(new File(configFilePath));
-        JSONArray serverSpecJSONArray = (JSONArray) new JSONParser().parse(fileReader);
+
+        JSONObject serverJSONObject = (JSONObject) new JSONParser().parse(fileReader);
+
+        if (!isContainServerConfigurationKey(serverJSONObject)) {
+            throw new JSONKeyNotFoundException();
+        }
+
+        this.mainLogPath = (String) serverJSONObject.get(MAIN_LOG_PATH);
+
+        JSONArray serverSpecJSONArray = (JSONArray) serverJSONObject.get(SERVER_SPEC);
 
         for (Object serverSpecObject : serverSpecJSONArray) {
             JSONObject serverSpecJSON = (JSONObject) serverSpecObject;
@@ -42,8 +55,12 @@ public class ServerConfiguration {
         }
     }
 
+    private static boolean isContainServerConfigurationKey(JSONObject jsonObject) {
+        return jsonObject.containsKey(MAIN_LOG_PATH) && jsonObject.containsKey(SERVER_SPEC);
+    }
+
     private static boolean isContainServerSpecKeys(JSONObject jsonObject) {
-        boolean isKeyExist = Arrays.stream(KEYS).allMatch(jsonObject::containsKey);
+        boolean isKeyExist = Arrays.stream(SERVER_SPEC_KEYS).allMatch(jsonObject::containsKey);
 
         if (!isKeyExist) {
             return false;
@@ -89,6 +106,10 @@ public class ServerConfiguration {
         serverSpecFinder
                 .get(serverSpec.getPortNumber())
                 .add(serverSpec);
+    }
+
+    public String getMainLogPath() {
+        return mainLogPath;
     }
 
     public List<ServerSpec> getServerSpecs(int portNumber) {
